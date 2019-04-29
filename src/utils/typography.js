a618e91c19c3928ecc80e2177e3cf8bd758d0e76
Tina Trenkner-Meade <tinatrenkner@gmail.com>
2019-04-29 21:48:44 +1200

    changes typography.js theme from WP2016 to Lincoln

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,17 +1,19 @@
 import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
+import lincolnTheme from "typography-theme-lincoln"
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    "a.gatsby-resp-image-link": {
-      boxShadow: `none`,
-    },
-  }
-}
 
-delete Wordpress2016.googleFonts
+lincolnTheme.overrideThemeStyles = () => {
+    googleFonts: [
+      {
+        name: 'Montserrat',
+        styles: [
+          '700',
+         ],
+      },
+    ],
+  
 
-const typography = new Typography(Wordpress2016)
+const typography = new Typography(lincolnTheme)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
