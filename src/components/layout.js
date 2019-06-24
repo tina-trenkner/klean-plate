82f00f2f6b7fa3a978de48ee63cc18d95f0ee3f1
Tina Trenkner-Meade <tinatrenkner@gmail.com>
2019-06-24 20:05:13 +1200

    creates menu component

--- src/components/layout.js
+++ src/components/layout.js
@@ -15,7 +15,7 @@ class Layout extends React.Component {
         <h1
           style={{
             ...scale(1.5),
-            marginBottom: rhythm(1.5),
+            // marginBottom: rhythm(1.5),
             marginTop: 0,
           }}
         >
--- src/components/menu.js
+++ src/components/menu.js
@@ -1,11 +1,23 @@
 import React from "react"
-
-//about
-
-//Archive
-
-//Food recs -- Instagram recs, vendor recs
+import { Link } from "gatsby"
 
 
+function Menu() {
+    return (
+      <div style={{display: `flex`}}>
+      <h2 style={{ marginTop: `0`}}>
+     <Link style={{
+       boxShadow: `none`,
+       textDecoration: `none`,
+       backgroundImage: `none`,
+     }} to={`recs`}
+      >
+      Food blogs, links, recs
+      </Link>
+
+     </h2>
+     </div>
+  )
+};
 
 export default Menu
